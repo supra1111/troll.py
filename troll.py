e87fc8e59cc1ff5b9cafc949bef35a153e0f9529
supra1111 <ylmaz686801@gmail.com>
2026-01-16 12:42:17 +0300

    Update troll.py

--- troll.py
+++ troll.py
@@ -166,3 +166,21 @@ async def zar(interaction: discord.Interaction):
 
 # ================= RUN =================
 bot.run(TOKEN)
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot aktif!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
